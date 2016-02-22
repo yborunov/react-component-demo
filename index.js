1aa32a24f0e4eb48496a6b62908200d12d80d1da
Yuri Borunov <yury@borunov.com>
2016-02-21 21:25:21 -0500

    index file

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentsList from 'containers/CommentsList.js'
+
+ReactDOM.render(<CommentsList />, document.getElementById('root'))
